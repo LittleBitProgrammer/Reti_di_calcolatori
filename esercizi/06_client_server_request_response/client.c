ae2c53df7d9187f20c63304d255972ad4bf05c15
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2022-09-30 20:12:15 +0200

    request & response
    request & response

---
+++ esercizi/06_client_server_request_response/client.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include "socket_utility.h"
+
+#define WRITER_BUFFER_SIZE 4096
+#define READER_BUFFER_SIZE 3
+
+int main(int argc, char**argv) {
+    /*
+     * ==========================
+     * =       Variables        =
+     * ==========================
+     * */
+    int                socket_file_descriptor;            /* socket client */
+    struct sockaddr_in server_address;                    /* Endpoint server */
+    struct hostent*    server_dns;                        /* structure dns of server */
+    char               writer_buffer[WRITER_BUFFER_SIZE]; /* Reader buffer */
+    char               reader_buffer[READER_BUFFER_SIZE]; /* writer buffer */
+
+    /*
+     * ==========================
+     * =       Arguments        =
+     * ==========================
+     * */
+    if(argc != 2)
+    {
+        fprintf(stderr, "usage: %s <IP/HOSTNAME>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * ==========================
+     * =          DNS           =
+     * ==========================
+     * */
+    if((server_dns = gethostbyname(argv[1])) == NULL)
+    {
+        herror("Errore DNS client");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * ==================================
+     * =        SOCKET CREATION         =
+     * ==================================
+     * */
+    socket_file_descriptor = Socket(AF_INET, SOCK_STREAM, 0);
+
+    /*
+     * ==================================
+     * =        SERVER CREATION         =
+     * ==================================
+     * */
+    server_address.sin_family = server_dns->h_addrtype;
+    server_address.sin_port = htons(13);
+    server_address.sin_addr = *((struct in_addr *)server_dns->h_addr_list[0]);
+
+    /*
+     * ==================================
+     * =           CONNECTION           =
+     * ==================================
+     * */
+    ConnectIPV4(socket_file_descriptor, &server_address);
+
+    /*
+     * ==================================
+     * =             INPUT              =
+     * ==================================
+     * */
+    printf("Inserire una stringa di caratteri qualsiasi:\n");
+    fgets(writer_buffer, WRITER_BUFFER_SIZE, stdin);
+
+    /*
+     * ==================================
+     * =            REQUEST             =
+     * ==================================
+     * */
+    FullWrite(socket_file_descriptor, writer_buffer, WRITER_BUFFER_SIZE);
+
+    /*
+     * ==================================
+     * =            RESPONSE            =
+     * ==================================
+     * */
+    FullRead(socket_file_descriptor, reader_buffer, READER_BUFFER_SIZE);
+
+    /*
+     * ==================================
+     * =            OUTPUT              =
+     * ==================================
+     * */
+    if(fputs(reader_buffer, stdout) == EOF)
+    {
+        fprintf(stderr, "fputs error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+}
---
+++ esercizi/06_client_server_request_response/socket_utility.h
@@ -0,0 +1,16 @@
+#ifndef SOCKET_UTILITY_H
+#define SOCKET_UTILITY_H
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+int Socket(int,int,int);
+void ConnectIPV4(int,struct sockaddr_in*);
+void BindIPV4(int,struct sockaddr_in*);
+void Listen(int,int);
+int AcceptIPV4(int, struct sockaddr_in*, socklen_t*);
+void PrintClientIPV4(struct sockaddr_in*, char *, socklen_t);
+size_t FullWrite(int, const void *, size_t);
+size_t FullRead(int, void *, size_t);
+
+#endif
