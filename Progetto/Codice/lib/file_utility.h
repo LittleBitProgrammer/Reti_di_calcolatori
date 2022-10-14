78f062fa0d3babf18cef9066035f17325026ad21
GaetanoIppolito <gaetano.ippolito001@studenti.uniparthenope.it>
2022-10-14 12:26:43 +0200

    Added "file_utility.h"
    - Added new library "file_utility.h"

---
+++ Progetto/Codice/lib/file_utility.h
@@ -0,0 +1,9 @@
+#ifndef FILE_UTILITY_H
+#define FILE_UTILITY_H
+
+#include "bool_utility.h"
+#define REGION_FILE_NAME "../file/client/tessera_sanitaria_codici_regione"
+
+bool is_code_written_in_file(char*, char*);
+
+#endif //FILE_UTILITY_H
