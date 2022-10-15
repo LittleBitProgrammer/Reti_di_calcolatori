348576fea820999c4df61eab173075453cc2fda6
GaetanoIppolito <gaetano.ippolito001@studenti.uniparthenope.it>
2022-10-15 12:06:10 +0200

    New function in "green_pass_utility.h"
    - Added new function

--- Progetto/Codice/lib/green_pass_utility.c
+++ Progetto/Codice/lib/green_pass_utility.c
@@ -1,1 +1,13 @@
 #include "green_pass_utility.h"
+
+void populate_reviser_response(Reviser_package* reviser_package)
+{
+    
+}
+
+bool check_green_pass_validity(struct tm* expiration_date, struct tm* today_date)
+{
+
+
+    return FALSE;
+}
--- Progetto/Codice/lib/green_pass_utility.h
+++ Progetto/Codice/lib/green_pass_utility.h
@@ -1,8 +1,11 @@
 #ifndef GREEN_PASS_UTILITY_H
 #define GREEN_PASS_UTILITY_H
 
+#include "package_utility.h"
+
 #define EXPIRATION_DURATION_MONTH 6
 
-
+void populate_reviser_response(Reviser_package*);
+bool check_green_pass_validity(struct tm*, struct tm*);
 
 #endif // GREEN_PASS_UTILITY_H
