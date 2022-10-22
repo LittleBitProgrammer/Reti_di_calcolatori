ed0bf9f66681cbd8ccc97cb73a9af253d6693b8d
Roberto Vecchio <roberto.vecchio93@hotmail.it>
2022-10-22 21:26:37 +0200

    fix
    fix

--- Progetto/Codice/central_server.c
+++ Progetto/Codice/central_server.c
@@ -13,6 +13,7 @@
  * ==========================
  */
 #include <stdio.h>
+#include <unistd.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <stdlib.h>
--- Progetto/Codice/vaccination_center_server.c
+++ Progetto/Codice/vaccination_center_server.c
@@ -14,6 +14,7 @@
  * ==========================
  */
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
